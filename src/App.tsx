9e09207a7261456fbc1c95c27d4c23698c254006
Md Abdul Mottalib <116445410+mdabdulmottalib@users.noreply.github.com>
2025-02-21 15:15:58 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import Login from './pages/Login';
 import Register from './pages/Register';
 import ForgotPassword from './pages/ForgotPassword';
 import TwoFactorAuth from './pages/TwoFactorAuth';
+import SuccessPage from './components/SuccessPage';
 
 function App() {
   return (
